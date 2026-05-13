6fb36439d8dbfd981fa9fe7f17952cd773294fb5
Saran-2308 <sksaran7071@gmail.com>
2026-05-13 09:09:07 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,7 +2,7 @@
 
 module tt_um_upcounter (
     input  wire [7:0] ui_in,
-    output reg  [7:0] uo_out,
+    output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
@@ -15,16 +15,14 @@ module tt_um_upcounter (
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            counter <= 4'b0000;
-        else if (ena)
+            counter <= 0;
+        else
             counter <= counter + 1'b1;
     end
 
-    always @(*) begin
-        uo_out = {4'b0000, counter};
-    end
+    assign uo_out = {4'b0000, counter};
 
-    assign uio_out = 8'b00000000;
-    assign uio_oe  = 8'b00000000;
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
