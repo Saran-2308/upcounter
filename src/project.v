1e4401b47b3681bcdb12164cf8e8a85776996035
Saran-2308 <sksaran7071@gmail.com>
2026-05-13 09:13:27 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,14 +15,14 @@ module tt_um_upcounter (
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            counter <= 0;
+            counter <= 4'b0000;
         else
             counter <= counter + 1'b1;
     end
 
     assign uo_out = {4'b0000, counter};
 
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
 endmodule
